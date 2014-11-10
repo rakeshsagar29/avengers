b13327bfa14fd99ca4cd3653d5aae52e5bd5b64d
RakeshSagar29 <sagar.rakesh.1986@gmail.com>
2014-11-10 16:37:59 +0530

    controller name modified and hero duplication is not allowed now.:

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var app = angular.module('avengersApp', []);
-app.controller('CharecterController',function($scope){
+app.controller('characterController',function($scope){
     $scope.hero =[];
     $scope.orderProp = 'name';
 
      $scope.saveCharecter = function(){
+
+        //Check for hero duplication
+        for(var i=0;i<$scope.hero.length;i++){
+            if($scope.name == $scope.hero[i].name)
+            {
+                $scope.name ="";
+                alert("There can't be two heroes with the same name.");  
+                return;
+            }
+
+        }
+
         $scope.hero.push({
             'name' : $scope.name,
             'weapon': $scope.weapon,
